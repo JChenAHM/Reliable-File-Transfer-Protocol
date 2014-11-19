834ae63bcf94107b3c854c0f8da5f8700f487d44
Jacob <chenshyfool@qq.com>
2014-11-18 23:00:13 -0600

    Create packet.h

---
+++ packet.h
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+
+#define TRUE    1
+#define FALSE   0
+
+#define PCKSIZE 5000
+#define BUFLEN 2048
+#define MSGS 5	/* number of messages to send */
+#define PCK_ROUND 2 /* number of packets sent per rounds */
+#define QUEUESIZE  100
+
